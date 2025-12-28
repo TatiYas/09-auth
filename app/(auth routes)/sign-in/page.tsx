25032f82d5007bb2efd17a40247ce472dc44f369
TatiYas <iasinovska_lawyer@ukr.net>
2025-12-28 17:38:49 +0100

    Create_sing-in/page

---
+++ app/(auth routes)/sign-in/page.tsx
@@ -0,0 +1,62 @@
+import { useState, FormEvent } from 'react';
+import css from './SignInPage.module.css';
+import { login, LoginRequest } from '@/lib/api/clientApi';
+import { isAxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/lib/store/authStore';
+
+export default function SignIn() {
+  const setUser = useAuthStore((state) => state.setUser);
+  const router = useRouter();
+  const [error, setError] = useState<string>('');
+
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // чтобы форма не перезагружала страницу
+    setError(''); // сброс ошибки при новом сабмите
+
+    const formData = new FormData(event.currentTarget);
+    const formValues = Object.fromEntries(formData.entries()) as LoginRequest;
+
+    try {
+      const res = await login(formValues);
+      if (res) {
+        setUser(res);
+        router.push('/profile');
+      } else {
+        setError('Invalid email or password');
+      }
+    } catch (err) {
+      if (isAxiosError(err)) {
+        setError(err.response?.data?.message || 'An error occurred. Please try again.');
+      } else {
+        setError('An unexpected error occurred. Please try again.');
+      }
+    }
+  };
+
+  return (
+    <main className={css.mainContent}>
+      <form className={css.form} onSubmit={handleLogin}>
+        <h1 className={css.formTitle}>Sign in</h1>
+
+        <div className={css.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" className={css.input} required />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" className={css.input} required />
+        </div>
+
+        <div className={css.actions}>
+          <button type="submit" className={css.submitButton}>
+            Log in
+          </button>
+        </div>
+
+        {error && <p className={css.error}>{error}</p>}
+      </form>
+    </main>
+  );
+}
