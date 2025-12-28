8308472a4897c23e4270a42745b0bb417ccbfb65
TatiYas <iasinovska_lawyer@ukr.net>
2025-12-28 20:39:56 +0100

    Update page.tsx

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -1,60 +1,35 @@
-import css from "./notesPage.module.css";
-import NoteListClient from "./Notes.client";
-import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
-import { fetchNotes } from "@/lib/api";
-import { Metadata } from "next";
-
+import type { Metadata } from 'next';
 
 type Props = {
-  params: Promise<{ slug: string[] }>;
+  params: {
+    slug: string[];
+  };
 };
-  
 
-export async function generateMetadata({ params }: Props): Promise<Metadata>  {
-  const {slug} = await params;
-  const tag = slug[0];
-  
-  
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const tag = params.slug?.[0] ?? 'all';
+
   const title = `Notes filtered by: ${tag}`;
   const description = `Review of notes filtered by "${tag}".`;
-  const url = `https://08-zustand-three-jet.vercel.app/notes/filter/${slug[0]}`;
+  const url = `https://09-auth-six-zeta.vercel.app/notes/filter/${tag}`;
 
   return {
     title,
     description,
-    openGraph: { 
+    openGraph: {
       title,
       description,
       url,
       siteName: 'NoteHub',
       images: [
-       {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-                width: 1200,
-        height: 630,
-        alt: "Title",
-      },
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'NoteHub preview',
+        },
       ],
       type: 'website',
     },
   };
-}
- 
-
-export default async function App({ params }: Props) {
-  const queryClient = new QueryClient();
-  const {slug} = await params;
-  const tag = slug[0] === 'All' ? undefined : slug[0];
-
-  queryClient.prefetchQuery({
-    queryKey: ['notes', {query: "", page: 1, tag: tag}],
-    queryFn: () => fetchNotes(1, "", tag),
-  });
-  return (
-    <div className={css.app}>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <NoteListClient tag={tag}/>
-      </HydrationBoundary>
-    </div>
-  );
 }
