2440a97fb1ce5894f4763a12460641bf93f7cbef
TatiYas <iasinovska_lawyer@ukr.net>
2025-12-29 00:58:35 +0100

    Update

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,59 @@
 
-export default function Loading (){
-    return(
-<div style={{ padding: "20px", textAlign: "center" }}>
-        <p>Loading, please wait...</p>
-      </div>)
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+import AuthProvider from "../components/AuthProvider/AuthProvider";
+
+export const metadata: Metadata = {
+  title: "Authenticated",
+  description: "NoteHub App",
+  openGraph: {
+    title: `NoteHub Auth`,
+    description:
+      "NoteHub Application Optimisation with React Query and Zustand",
+    url: `https://09-auth-ashy-three.vercel.app/`,
+    siteName: "NoteHub",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub",
+      },
+    ],
+    type: "article",
+  },
+};
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+  modal,
+}: Readonly<{
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.variable}>
+        <TanStackProvider>
+          <AuthProvider>
+            <Header />
+            {modal}
+            {children}
+            <Footer />
+          </AuthProvider>
+        </TanStackProvider>
+      </body>
+    </html>
+  );
 }
