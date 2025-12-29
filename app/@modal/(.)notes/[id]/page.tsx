9d7c98a25432faeda03f31bcf5b51937bf87e76e
TatiYas <iasinovska_lawyer@ukr.net>
2025-12-29 23:34:22 +0100

    Update page.tsx

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -7,11 +7,11 @@ import { fetchServerNoteById } from "@/lib/api/serverApi";
 import NotePreviewClient from "./NotePreview.client";
 
 type NoteDetailsPageProps = {
-  params: { id: string };
+  params: Promise<{ id: string }>; // ту ту ту виправила
 };
 
 export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
-  const { id } = params;
+  const { id } = await params; // <-- тра ля ля використовую await
 
   const queryClient = new QueryClient();
 
