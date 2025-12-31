df193fa0f54b44a203c93b15a79328570875c0f2
TatiYas <iasinovska_lawyer@ukr.net>
2025-12-31 04:00:09 +0100

    Update page.tsx

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -12,7 +12,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
   const title = `Notes filtered by: ${tag}`;
   const description = `Review of notes  "${tag}".`;
-  const url = `https://09-auth-1vfd.vercel.app/notes/filter/${params.slug[0]}`;
+  const url = `https://09-auth-ten-tan.vercel.app//notes/filter/${params.slug[0]}`;
 
   return {
     title,
