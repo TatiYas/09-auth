25f82fe3aa7eb2a96fbf1f7250e02a11466a0471
TatiYas <iasinovska_lawyer@ukr.net>
2025-12-31 01:35:55 +0100

    Update

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-type Props = {
+export default function AuthLayout({
+  children,
+}: {
   children: React.ReactNode;
-};
-
-export default function PublicLayout({ children }: Props) {
+}) {
   const [loading, setLoading] = useState(true);
-
   const router = useRouter();
 
   useEffect(() => {
     router.refresh();
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setLoading(false);
   }, [router]);
 
-  return <>{loading ? <div>Loading... Please wait.</div> : children}</>;
+  return <>{loading ? <div>Loading...</div> : children}</>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,75 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+import AuthProvider from  '../components/AuthProvider/AuthProvider'
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+weight: ['400', '700'],
+display: 'swap',
+});
+
+
+export const metadata: Metadata = {
+  title: "Authenticated",
+  description: "NoteHub App.",
+  openGraph: {
+    title: "NoteHub Auth",
+    description: "NoteHub Application Optimisation with React Query and Zustand",
+    url: "https://09-auth-wo74.vercel.app/",
+    siteName: "NoteHub",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+
+export default function RootLayout({
+  children,
+  modal,
+}: Readonly<{
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.variable}>
+       <TanStackProvider>
+          <AuthProvider>
+            <Header />
+            {children}
+            {modal}
+            <Footer />
+          </AuthProvider>
+        </TanStackProvider>
+      </body>
+    </html>
+  );
+}
+
+
+
+
+/**import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
 
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
-/* import AuthNavigation from "@/components/AuthNavigation/AuthNavigation"; */
+/* import AuthNavigation from "@/components/AuthNavigation/AuthNavigation"; 
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -55,4 +118,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+}*/
