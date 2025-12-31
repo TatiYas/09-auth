c099d78bd0aa0b610beae65638875785e2f7c48d
TatiYas <iasinovska_lawyer@ukr.net>
2025-12-31 03:05:16 +0100

    Update page.tsx

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -1,4 +1,74 @@
+
 import NoteListClient from "./Notes.client";
+import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { fetchServerNotes } from "@/lib/api/serverApi";
+import { Metadata } from "next";
+
+type Props = {
+  params: { slug: string[] };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const tag = await params.slug[0];
+
+  const title = `Notes filtered by: ${tag}`;
+  const description = `Review of notes  "${tag}".`;
+  const url = `https://09-auth-1vfd.vercel.app/notes/filter/${params.slug[0]}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Title",
+        },
+      ],
+      type: 'website',
+    },
+  };
+}
+
+export default async function App({ params }: Props) {
+  const queryClient = new QueryClient()
+	const { slug } = await params
+	const tag = slug[0] === "All" ? undefined : slug[0]
+
+
+  await queryClient.prefetchQuery({
+    queryKey: ['notes', { query: "", page: 1, tag: tag }],
+    queryFn: () => fetchServerNotes(1, "", tag ),
+  });
+
+  return (
+    <div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <NoteListClient tag={tag}   />
+      </HydrationBoundary>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import NoteListClient from "./Notes.client";
 import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { fetchServerNotes } from "@/lib/api/serverApi";
 import { Metadata } from "next";
@@ -53,7 +123,7 @@ export default async function App({ params }: Props) {
       </HydrationBoundary>
     </div>
   );
-}
+}*/
 
 
 
